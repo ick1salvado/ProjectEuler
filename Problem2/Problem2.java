021a66ab364c6542742652a7ca33d7312ccd57c4
ick1salvado <52610756+ick1salvado@users.noreply.github.com>
2022-10-26 17:56:42 -0500

    Delete Problem2 directory

--- Problem2/Problem2.java
+++
@@ -1,9 +0,0 @@
-//Even Fibonacci Numbers
-
-public class Main{
-    public static void main(String[] args){
-        int sum = 0;
-        for(int i=1,j=2,temp;j<4000000;temp=j,j+=i,i=temp) if(j % 2 == 0) sum += j;
-        System.out.println(sum);
-    }
-}
--- Problem2/Problem2.py
+++
@@ -1,7 +0,0 @@
-#Even Fibonacci Numbers
-i,j,s = 1,2,0
-
-while i < 4000000:
-    if(j % 2 == 0): s = s + j
-    i,j = j,i+j
-print(s)
